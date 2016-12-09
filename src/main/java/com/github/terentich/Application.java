8ccf3eaabdcb4c1a7fa7d9b84e6eae25f14030fe
aterentyev <terentich@gmail.com>
2016-12-09 18:29:17 +0300

    Add support for the Spring Boot Admin.

--- src/main/java/com/github/terentich/Application.java
+++ src/main/java/com/github/terentich/Application.java
@@ -1,9 +1,11 @@
 package com.github.terentich;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
