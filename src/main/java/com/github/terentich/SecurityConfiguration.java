1b22b1b7f144581bcf746493e577eac025bd4456
aterentyev <terentich@gmail.com>
2017-04-12 01:41:11 +0300

    Implement admin (predefined) and user (Facebook) security.

---
+++ src/main/java/com/github/terentich/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.github.terentich;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * Created by aterentyev on 11.04.2017.
+ */
+@Configurable
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Configuration
+    @Order(1)
+    public static class AdminSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .antMatchers("/actuator/health").permitAll()
+//                    .antMatchers("/admin/**", "/actuator/**").authenticated()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf().ignoringAntMatchers("/admin/api/**")
+                    .and()
+                    .httpBasic()
+            ;
+        }
+
+//        @Override
+//        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//            auth.inMemoryAuthentication().and().ldapAuthentication();
+//        }
+    }
+
+//
+//    @Configuration
+//    @Order(2)
+//    public static class UserSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http
+//                    .authorizeRequests()
+//                    .antMatchers("/**").authenticated()
+//            .and()
+//            .httpBasic();
+//        }
+//    }
+}
