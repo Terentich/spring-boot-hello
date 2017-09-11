337aa57c2953f5da2e5633fcdecffd4774c2fdc9
aterentyev <terentich@gmail.com>
2017-09-11 13:47:31 +0300

    Introduce security tests.

---
+++ src/test/java/AuthenticationTests.java
@@ -0,0 +1,81 @@
+import com.github.terentich.Application;
+import de.codecentric.boot.admin.event.ClientApplicationEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthenticationTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void unauthorizedRoot() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void unauthorizedApi() throws Exception {
+        mvc.perform(get("/api"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void unauthorizedAdmin() throws Exception {
+        mvc.perform(get("/admin"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void unauthorizedActuator() throws Exception {
+        mvc.perform(get("/actuator"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    public void authorizedRoot() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void authorizedApi() throws Exception {
+        mvc.perform(get("/api"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Success"));
+    }
+
+    @Test
+    @WithMockUser
+    public void authorizedAdmin() throws Exception {
+        mvc.perform(get("/admin/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void authorizedActuator() throws Exception {
+        mvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk());
+    }
+}
